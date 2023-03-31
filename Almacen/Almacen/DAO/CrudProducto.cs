c434273fe2563388f0ddd93e4033a55e8fe0fad7
Ronnie1501 <124007445+Ronnie1501@users.noreply.github.com>
2023-03-31 04:38:03 -0600

    copiar codigo

---
+++ Almacen/Almacen/DAO/CrudProducto.cs
@@ -0,0 +1,27 @@
+﻿using Almacen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Almacen.DAO
+{
+    internal class CrudProducto
+    {
+        public void AgregarProducto(Producto ParametroProducto)
+        {
+            using (AlmacenContext db = new AlmacenContext())
+            {
+                Producto producto = new Producto();
+                producto.Nombre = ParametroProducto.Nombre;
+                producto.Descripción = ParametroProducto.Descripción;
+                producto.Precio = ParametroProducto.Precio;
+                producto.Stock = ParametroProducto.Stock;
+                db.Add(producto);
+                db.SaveChanges();
+
+            }
+        }
+    }
+}
---
+++ Almacen/Almacen/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Almacen.Models;
+using Almacen.DAO;
+using System;
+
+CrudProducto CrudProducto = new CrudProducto();
+Producto Producto = new Producto();
+
+Console.WriteLine("Menu");
+Console.WriteLine("pulse 1 para insertar un nuevo producto");
+Console.WriteLine("pulse 0 para cancelar");
+var Menu = Convert.ToInt32(Console.ReadLine());
+
+switch (Menu)
+{
+    case 1:
+        int bucle = 1;
+        while (bucle == 1)
+        {
+            Console.WriteLine("Ingrese el nombre del producto:");
+            Producto.Nombre = Console.ReadLine();
+            Console.WriteLine("ingrese la descripcion del producto");
+            Producto.Descripción = Console.ReadLine();
+            Console.WriteLine("ingrese el precio del producto 00.00");
+            Producto.Precio = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("ingrese la cantidad en stock");
+            Producto.Stock = Convert.ToInt32(Console.ReadLine());
+            CrudProducto.AgregarProducto(Producto);
+            Console.WriteLine("Se agregó correctamente el producto");
+            Console.WriteLine("pulsa 1 para agregar un producto");
+            Console.WriteLine("pulsa 0 para salir");
+            bucle = Convert.ToInt32(Console.ReadLine());
+        }
+        break;
+
+}
